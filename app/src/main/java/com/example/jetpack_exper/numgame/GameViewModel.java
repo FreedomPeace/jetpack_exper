42a97fb5bd510ee52c230b46effac620b7e59b7d
aizhiping <aizp@bankcomm.com>
2020-04-13 15:58:41 +0800

    自定义的view也可以用单向绑定

--- app/src/main/java/com/example/jetpack_exper/numgame/GameViewModel.java
+++ app/src/main/java/com/example/jetpack_exper/numgame/GameViewModel.java
@@ -2,6 +2,7 @@ package com.example.jetpack_exper.numgame;
 
 import android.app.Application;
 import android.text.TextUtils;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -39,6 +40,8 @@ public class GameViewModel extends AndroidViewModel{
         num2.setValue("");
         num3.setValue("");
         num4.setValue("");
+        express.setValue("");
+        gameNum.setValue("");
     }
 
     public String expressValue() {
@@ -62,7 +65,10 @@ public class GameViewModel extends AndroidViewModel{
         if (!TextUtils.isEmpty(value)) {
             Card36.rightResult = Integer.parseInt(value);
         }
+        long l = System.currentTimeMillis();
         boolean hasResult = Card36.getCard36ByInputNum(cards, true);
+        long l2 = System.currentTimeMillis();
+        Log.d("getCard36ByInputNum", l2 - l+"");
         if (hasResult) {
 //            Toast.makeText(getApplication(), "bingo ，这个可以算出"+Card36.rightResult+"的值", Toast.LENGTH_LONG)
 //                    .show();
